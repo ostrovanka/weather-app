823eff9c6a241609bd7cb547e7bf08833fd2178a
Anna Ostrovska <anna.ostrovska@yum.com>
2022-03-11 13:25:08 +0200

    Change footer

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,9 @@ const forecastParagraph = document.querySelector('#forecast');
 
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
+    fetch("https://api.bmg.tallium.com/v1/media").then(response=>{
+        console.log(JSON.stringify(response))
+    })
     infoParagraph.textContent='Searching location...'
     forecastParagraph.textContent=''
     fetch(`/weather?address=${addressInput.value}`).then((response)=>{
